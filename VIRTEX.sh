18f2953bc1f4ffd0c09a371d5252f173716fb70b
Rizqi Mbmbg <97333730+Riskimbmbg@users.noreply.github.com>
2022-12-01 12:00:50 +0700

    VIRTEX-GANAS.sh

---
+++ VIRTEX.sh
@@ -0,0 +1,263 @@
+clear
+#logo
+
+echo "#########################################################"
+echo "#                                                       #"
+echo "#                  Author : Riski Ganzz                 #"
+echo "#                  Github : Riskimbmbg                  #"
+echo "#                 Youtube : riskimbmbg                  #"
+echo "#                                                       #"
+echo "#########################################################"
+echo
+sleep 3
+echo "       [ↆ] DOWNLOAD TOKEN DULU SEBELUM MASUK [ↆ]"
+echo "   https://www.mediafire.com/download/wkictn43qhfk9c0"
+echo
+sleep 3
+echo
+#password
+        read -p "Sudah Download Token? KETIK Y/y " username
+        read -p "[∆] Masukan Tokenya [∆] : " pass
+
+echo $pass $username
+sleep 3
+echo
+if [ $pass = "Virtexrizqi" ]
+        then
+	echo [•]"Mengecek Token Yang Di Masukan"
+	sleep 2
+	echo [•]"Mohon Tunggu"
+	sleep 5
+        echo [✓]"Token Valid"
+        sleep 3
+clear
+        else
+	echo [•]"Mengecek Token Yang Di Masukan"
+        sleep 2
+        echo [•]"Mohon Tunggu"
+        sleep 5
+        echo [X]"Token Salah"
+        echo [X]"Ulangi Lagi"
+        sleep 3
+        bash VIRTEX.sh
+        sleep 4
+
+fi
+sleep 2
+
+#logo
+
+echo "#########################################################"
+echo "#                                                       #"
+echo "#                  Author : Riski Ganzz                 #"
+echo "#                  Github : Riskimbmbg                  #"
+echo "#                 Youtube : riskimbmbg                  #"
+echo "#                                                       #"
+echo "#########################################################"
+echo
+sleep 3
+	echo "Jangan Di Salah Gunakan Ingat!"
+	sleep 4
+	echo
+ 	echo "--------Menu Script!--------"
+	sleep 5
+        echo
+        echo [1]"‣ Download Virtex 1"
+        echo [2]"‣ Download Virtex 2"
+        echo [3]"‣ Donasi Ke Admin"
+        echo [4]"‣ Jadi Member Vip"
+        echo [0]"‣ Exit Script"
+        echo
+        read -p "PILIHANMU ‣ : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+
+echo "#########################################################"
+echo "#                                                       #"
+echo "#                  Author : Riski Ganzz                 #"
+echo "#                  Github : Riskimbmbg                  #"
+echo "#                 Youtube : riskimbmbg                  #"
+echo "#                                                       #"
+echo "#########################################################"
+echo
+sleep 2
+echo
+echo $"Mencari Virtex..."
+sleep 4
+echo $"Loading..."
+echo $"Keluar Dengan CTRL+Z"
+sleep 3
+echo
+echo "Rizqi Virtex sudah di temukan!"
+sleep 3
+echo
+echo "___________________________________________________"
+echo
+echo "https://www.mediafire.com/download/jpp2w85lp34i1rf"
+echo "___________________________________________________"
+echo
+sleep 3
+echo "Download Virtex Dan Buka File Di Chrome"
+echo "Setelah Di Buka Salin Virtex Dan Jangan Di Ubah Akan Eror"
+echo "Tempel Ke WhatsApp Kebalmu Dan Kirim"
+fi
+
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+
+echo "#########################################################"
+echo "#                                                       #"
+echo "#                  Author : Riski Ganzz                 #"
+echo "#                  Github : Riskimbmbg                  #"
+echo "#                 Youtube : riskimbmbg                  #"
+echo "#                                                       #"
+echo "#########################################################"
+echo
+sleep 2
+echo
+echo $"Mencari Virtex..."
+sleep 4
+echo $"Loading..."
+echo $"Keluar Dengan CTRL+Z"
+sleep 3
+echo
+echo "Rizqi Virtex sudah di temukan!"
+sleep 3
+echo
+echo "___________________________________________________"
+echo
+echo "https://www.mediafire.com/download/c8l37baqhbbi7ww"
+echo "___________________________________________________"
+echo
+sleep 3
+echo "Download Virtex Dan Buka File Di Chrome"
+echo "Setelah Di Buka Salin Virtex Dan Jangan Di Ubah Akan Eror"
+echo "Tempel Ke WhatsApp Kebalmu Dan Kirim"
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+
+echo "#########################################################"
+echo "#                                                       #"
+echo "#                  Author : Riski Ganzz                 #"
+echo "#                  Github : Riskimbmbg                  #"
+echo "#                 Youtube : riskimbmbg                  #"
+echo "#                                                       #"
+echo "#########################################################"
+echo
+sleep 2
+echo
+echo "TERMIKASIH BANYAK BRE UDAH MAU DONASI"
+echo "Kirim Pulsa Aja Bre"
+echo
+echo "Nomer = 081387603153"
+echo
+sleep 2
+echo "Thanks Bro"
+echo "Sampai Jumpa Di Script Gua Selanjutnya"
+echo
+echo
+echo
+echo "Requests Script? 5000 Ya Bre"
+echo "Wa Aja Di Nomer Di Atas"
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+
+echo "#########################################################"
+echo "#                                                       #"
+echo "#                  Author : Riski Ganzz                 #"
+echo "#                  Github : Riskimbmbg                  #"
+echo "#                 Youtube : riskimbmbg                  #"
+echo "#                                                       #"
+echo "#########################################################"
+echo
+sleep 2
+echo "VIP MEMBER BARU"
+echo "_______ ________ _______ ________ ________ _______"
+echo
+echo "Daftar Sekarang Dan Bisa Request Script Sesukamu"
+sleep 2
+echo "Wa Langsung Aja Bre"
+echo "Wa.me/6281387603153"
+echo
+echo "_______ ________ _______ ________ ________ _______"
+echo
+sleep 3
+echo
+echo "DAFTAR REQUESTS SCRIPT DAN HARGA"
+
+sleep 1
+echo
+echo "[✓] HACK FACEBOOK         [50K Via Pulsa]"
+sleep 1
+echo "[✓] HACK INSTAGRAM        [50K Via Pulsa]"
+sleep 1
+echo "[✓] HACK MOBILE LEGEND    [35K Via Pulsa]"
+sleep 1
+echo "[✓] HACK FREE FIRE        [35K Via Pulsa]"
+sleep 1
+echo "[✓] HACK CLASH OF CLANS   [20K Via Pulsa]"
+sleep 1
+echo "[✓] HACK PUBG MOBILE      [25K Via Pulsa]"
+sleep 1
+echo "[✓] HACK TIK TOK          [15K Via Pulsa]"
+sleep 1
+echo "[✓] HACK CLASH ROYAL      [7K  Via Pulsa]"
+sleep 1
+echo "[✓] HACK RICE OF KINGDOMS [5K  Via Pulsa]"
+sleep 1
+echo "[✓] HACK LORDS MOBILE     [10K Via Pulsa]"
+sleep 1
+echo "[✓] HACK HIGGS SLOT       [50K Via Pulsa]"
+sleep 1
+echo "[✓] HACK DOMINO ISLAND    [50K Via Pulsa]"
+sleep 1
+echo "[✓] HACK YouTube          [70K Via Pulsa]"
+sleep 1
+echo "[✓] HACK AKUN GOOGLE      [100K Via Pulsa]"
+sleep 1
+echo "[✓] HACK AKUN EMAIL       [50K Via Pulsa]"
+sleep 1
+echo "[✓] AOV MOBILE            [10K Via Pulsa]"
+sleep 2
+echo
+echo "_______ ________ _______ ________ ________ _______"
+sleep 2
+echo "HARGA BERBEDA BEDA TERGANTUNG KESULITAN PEMBUATANYA"
+sleep 1
+echo "MEMBER VIP BISA REQUESTS 1 SCRIPT"
+fi
+
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+
+echo "#########################################################"
+echo "#                                                       #"
+echo "#                  Author : Riski Ganzz                 #"
+echo "#                  Github : Riskimbmbg                  #"
+echo "#                 Youtube : riskimbmbg                  #"
+echo "#                                                       #"
+echo "#########################################################"
+echo
+sleep 2
+echo "SELAMAT TINGGAL BRE"
+echo "MAKASIH SUDAH MENGGUNAKAN SCRIPT SAYA"
+echo "_______ ________ _______ ________ ________ _______"
+echo
+echo "JADI MEMBER VIP DAN NIKMATI SCRIPT SESUKAMU"
+echo "_______ ________ _______ ________ ________ _______"
+echo
+echo "GOOD BYE"
+exit
+fi
